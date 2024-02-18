32e31447ef74a59538a9fe489abd09fd9b3659bd
Artur Voloshin <nozeilwrk@gmail.com>
2024-02-18 15:41:04 +0300

    feat: implement login form validation

--- src/components/forms/login-form/login-form.tsx
+++ src/components/forms/login-form/login-form.tsx
@@ -1,59 +1,86 @@
+import { useState } from 'react';
 import { GooglePlusOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import { Link } from 'react-router-dom';
+import { emailPattern, passwordPattern } from '../regex';
 
 import styles from './../forms.module.css';
 
-const LoginForm = () => (
-    <Form
-        className={styles.form}
-        name='sign-in-form'
-        initialValues={{ remember: true }}
-        autoComplete='off'
-        size='large'
-    >
-        <Form.Item
-            className={styles.email}
-            name='email'
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your email!',
-                },
-            ]}
-        >
-            <Input className={styles.input} type='email' addonBefore='e-mail:' />
-        </Form.Item>
+const LoginForm = () => {
+    const [form] = Form.useForm();
+    const [isDisabled, setIsDisabled] = useState(false);
 
-        <Form.Item
-            className={styles.password}
-            name='password'
-            rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-            <Input.Password className={styles.input} placeholder='Пароль' />
-        </Form.Item>
+    const onFieldsChange = () => {
+        const hasErrors = form.getFieldsError().some(({ errors }) => errors.length);
+        setIsDisabled(hasErrors);
+    };
 
-        <Form.Item className={styles.checkArea}>
-            <Form.Item name='remember' valuePropName='defaultChecked' noStyle>
-                <Checkbox>Запомнить меня</Checkbox>
+    return (
+        <Form
+            className={styles.form}
+            form={form}
+            name='sign-in-form'
+            initialValues={{ remember: true }}
+            autoComplete='off'
+            size='large'
+            onFieldsChange={onFieldsChange}
+        >
+            <Form.Item
+                className={styles.email}
+                name='email'
+                rules={[
+                    {
+                        required: true,
+                        pattern: emailPattern,
+                        message: '',
+                    },
+                ]}
+            >
+                <Input className={styles.input} type='email' addonBefore='e-mail:' />
             </Form.Item>
 
-            <Button className={styles.forgotPasswordBtn} type='link'>
-                <Link to=''>Забыли пароль?</Link>
-            </Button>
-        </Form.Item>
+            <Form.Item
+                className={styles.password}
+                name='password'
+                rules={[
+                    {
+                        required: true,
+                        pattern: passwordPattern,
+                        message: '',
+                    },
+                ]}
+            >
+                <Input.Password className={styles.input} placeholder='Пароль' />
+            </Form.Item>
 
-        <Form.Item className={styles.loginBtnFormItem}>
-            <Button className={styles.btn} block type='primary' htmlType='submit'>
-                Войти
-            </Button>
-        </Form.Item>
-        <Form.Item>
-            <Button className={styles.btn} block htmlType='submit' icon={<GooglePlusOutlined />}>
-                Войти через Google
-            </Button>
-        </Form.Item>
-    </Form>
-);
+            <Form.Item className={styles.checkArea}>
+                <Form.Item name='remember' valuePropName='defaultChecked' noStyle>
+                    <Checkbox>Запомнить меня</Checkbox>
+                </Form.Item>
+
+                <Button className={styles.forgotPasswordBtn} type='link'>
+                    <Link to=''>Забыли пароль?</Link>
+                </Button>
+            </Form.Item>
+
+            <Form.Item className={styles.loginBtnFormItem}>
+                <Button
+                    className={styles.btn}
+                    block
+                    type='primary'
+                    htmlType='submit'
+                    disabled={isDisabled}
+                >
+                    Войти
+                </Button>
+            </Form.Item>
+            <Form.Item>
+                <Button className={styles.btn} block icon={<GooglePlusOutlined />}>
+                    Войти через Google
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
 
 export default LoginForm;
