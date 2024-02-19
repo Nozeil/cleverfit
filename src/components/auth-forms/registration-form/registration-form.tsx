034787c3a0b3bb025213a32154452677b2b64c5d
Artur Voloshin <nozeilwrk@gmail.com>
2024-02-19 16:22:19 +0300

    refactor: move css variables to separate files

--- src/components/auth-forms/registration-form/registration-form.tsx
+++ src/components/auth-forms/registration-form/registration-form.tsx
@@ -4,13 +4,15 @@ import PasswordInput from '../components/inputs/password-input';
 
 import styles from './registration-form.module.css';
 
+const helpMessage = 'Пароль не менее 8 символов, с заглавной буквой и цифрой';
+
 const RegistrationForm = () => (
     <AuthForm name='registration-form' googleButtonText='Регистрация через Google'>
         <div className={styles.registrationFormGroup}>
             <EmailInput />
             <PasswordInput
-                help='Пароль не менее 8 символов, с заглавной буквой и цифрой'
-                message='Пароль не менее 8 символов, с заглавной буквой и цифрой'
+                help={helpMessage}
+                message={helpMessage}
             />
             <PasswordInput name='password-repeat' placeholder='Повторите пароль' />
         </div>
