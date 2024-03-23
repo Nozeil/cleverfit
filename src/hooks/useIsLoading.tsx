72542fbef1d55be46473008c0d9abb24f5415c7f
Artur Voloshin <nozeilwrk@gmail.com>
2024-03-23 18:11:05 +0300

    fix: disable loader on user endpoints

--- src/hooks/useIsLoading.tsx
+++ src/hooks/useIsLoading.tsx
@@ -5,11 +5,21 @@ const status = 'pending';
 export const useIsLoading = () => {
     const api = useAppSelector((state) => state.api);
 
-    const isQueryPending = Object.values(api.queries).some((query) => query?.status === status);
-    const isMutatitonPending = Object.values(api.mutations).some((mutation) => {
-        if (mutation?.endpointName === 'createTraining') {
+    const isQueryPending = Object.values(api.queries).some((query) => {
+        if (query?.endpointName === 'getUserInfo') {
             return false;
         }
+
+        return query?.status === status;
+    });
+    const isMutatitonPending = Object.values(api.mutations).some((mutation) => {
+        if (
+            mutation?.endpointName === 'createTraining' ||
+            mutation?.endpointName === 'updateUserInfo'
+        ) {
+            return false;
+        }
+
         return mutation?.status === status;
     });
 
